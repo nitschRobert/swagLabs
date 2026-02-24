8d6cd944918b27f0ea3a6791ec507e501ceccaec
nitschRobert <nicz.robert@gmail.com>
2026-02-24 16:32:17 +0100

    Improve GitHub Actions test results visibility

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html'], ['junit', { outputFile: 'test-results/junit.xml' }]],
   use: {
     baseURL: 'https://www.saucedemo.com/',
     trace: 'retain-on-failure',
